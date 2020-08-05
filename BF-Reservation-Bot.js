e2ef461133f01fb3210cdd851af3aa0fb634bd88
Valentin Di Natale <valdinatale@DIPOSTEL.LOCAL>
2020-08-05 11:59:57 +0200

    Add warning message at starting of the script

--- BF-Reservation-Bot.js
+++ BF-Reservation-Bot.js
@@ -3,6 +3,7 @@ const bf = require('./basic-fit');
  
 (async () => {
 
+    console.log('Dont forget to put and remove your pass in secret.js !');
     await bf.initialize();
     await bf.login(secret);
     await bf.booking();
